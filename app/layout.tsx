7dbb44693b304d9fa3a48f00e530f778dd771266
Cry <m.f.zuravlev@yandex.ru>
2023-02-01 19:27:28 +0500

    added some config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css'
+import globalMetadata from './metadata'
+
+export const metadata = globalMetadata
 
 export default function RootLayout({
   children,
@@ -6,13 +9,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang='ru'>
       <head />
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
