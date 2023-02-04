b15273b4f05c20c92f3969f696c0f85bd275dc79
Cry <m.f.zuravlev@yandex.ru>
2023-02-04 19:14:30 +0500

    создал разметку главной и постов

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from 'components/footer'
+import Header from 'components/header'
 import './globals.css'
 import globalMetadata from './metadata'
 
@@ -11,8 +13,10 @@ export default function RootLayout({
   return (
     <html lang='ru'>
       <head />
-      <body className='w-full'>
-        <main className='px-2 md:px-0'>{children}</main>
+      <body className='flex flex-col items-center w-full h-auto border'>
+        <Header />
+        <main className='w-full max-w-6xl h-auto px-2 md:px-0'>{children}</main>
+        <Footer />
       </body>
     </html>
   )
