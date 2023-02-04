65402facedc3f0a3d001c7aa63ed77836d52c9bf
Cry <m.f.zuravlev@yandex.ru>
2023-02-04 22:35:46 +0500

    изменил стили и добавил структуру карточки поста

--- app/page.tsx
+++ app/page.tsx
@@ -15,33 +15,195 @@ export const metadata = {
 
 export default function HomePage() {
   return (
-    <section className='grid grid-flow-row-1 md:grid-flow-row-3 md:grid-flow-col md:gap-x-24 gap-y-12 md:gap-y-16 w-full items-start pt-16'>
-      <div className='w-full col-span-2 row-span-3'>
-        <ul className='flex flex-col w-full'>
-          <li className='w-full h-52 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-          <li className='w-full h-52 mt-12 border'></li>
-        </ul>
+    <section className='flex flex-col'>
+      <div className='grid grid-flow-row-1 md:grid-flow-row-3 md:grid-flow-col md:gap-x-24 gap-y-12 md:gap-y-16 w-full items-start pt-16'>
+        <div className='w-full col-span-2 row-span-3'>
+          <ul className='flex flex-col w-full'>
+            <li className='w-full h-52'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+            <li className='w-full h-52 mt-12'>
+              <article className='text-black'>
+                <a href='#' className='flex flex-col'>
+                  <h3 className='w-40 h-10 border'></h3>
+                  <p className='w-full h-28 mt-4 border'></p>
+                  <div className='w-full h-7 mt-2 border'></div>
+                </a>
+              </article>
+            </li>
+          </ul>
+        </div>
+        <div className='w-full h-36 col-span-2 md:col-span-1 border'></div>
+        <div className='md:sticky w-full h-96 row-span-1 md:row-span-1 col-span-2 md:col-span-1 border'></div>
       </div>
-      <div className='w-full h-36 col-span-2 md:col-span-1 border'></div>
-      <div className='md:sticky w-full h-96 row-span-1 md:row-span-1 col-span-2 md:col-span-1 border'></div>
     </section>
   )
 }
