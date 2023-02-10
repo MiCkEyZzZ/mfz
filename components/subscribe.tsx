440e8b01e7349d59f5169496be1c64a15da960d6
Cry <m.f.zuravlev@yandex.ru>
2023-02-11 01:37:17 +0500

    добавил атрибуты

--- components/subscribe.tsx
+++ components/subscribe.tsx
@@ -3,10 +3,10 @@ function Subscribe(): JSX.Element {
     <section className='flex flex-col items-center w-full my-24 px-5 md:px-5'>
       <div className='flex flex-col justify-between w-full max-w-6xl'>
         <div className='w-full max-w-xl'>
-          <h2 className='text-4xl mb-6'>
+          <h2 className='text-2xl md:text-3xl lg:text-4xl mb-6'>
             Каждый день выходит, что-то новое - не пропустите интересное.
           </h2>
-          <p className='text-2xl'>
+          <p className='text-base md:text-xl lg:text-2xl'>
             Подпишись на рассылку новостей и будь в курсе о выходе новых
             материалов.
           </p>
@@ -21,7 +21,10 @@ function Subscribe(): JSX.Element {
               />
             </fieldset>
             <div className='mt-5 ml-auto md:ml-5 md:mt-0'>
-              <button className='px-5 md:px-7 py-3 md:py-5 border rounded-md'>
+              <button
+                aria-label='Подписаться'
+                className='px-5 md:px-7 py-3 md:py-5 border rounded-md'
+              >
                 Подписаться
               </button>
             </div>
