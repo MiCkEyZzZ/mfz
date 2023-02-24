9e9765d60aaac85133079dfa860013ff2f9e433b
Cry <m.f.zuravlev@yandex.ru>
2023-02-24 15:31:03 +0500

    добавил ф-ю склонения

--- app/posts/nextjs/page.tsx
+++ app/posts/nextjs/page.tsx
@@ -1,5 +1,6 @@
 import { PostCard } from 'components'
 import { allPosts } from 'contentlayer/generated'
+import { getDeclensionsWorld } from 'lib/helper'
 
 function getPosts(type: string) {
   return allPosts.filter((post) => post.tag === type)
@@ -8,13 +9,16 @@ function getPosts(type: string) {
 function ReactPage() {
   const posts = getPosts('next.js')
   const count = posts.length
+  const postWord = getDeclensionsWorld(count)
 
   return (
     <section className='flex flex-col w-full max-w-6xl pt-32'>
       <div className='flex flex-col'>
         <div className='flex flex-row justify-between items-center w-full h-20 pl-5 pr-10'>
           <h1 className='text-4xl font-semibold'>Next.js</h1>
-          <p className='hidden sm:block text-lg font-normal'>{count} статей</p>
+          <p className='hidden sm:block text-lg font-normal'>
+            {count} {postWord}
+          </p>
         </div>
         <ul className='grid grid-cols-1 md:grid-cols-auto gap-8'>
           {posts.map((post) => (
