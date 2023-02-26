1d229269d572b560cf6a30baefaee936a6125c3c
Cry <m.f.zuravlev@yandex.ru>
2023-02-26 14:29:12 +0500

    исправил ошибку и добавил ключ

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -39,7 +39,7 @@ function BlogPage(): JSX.Element {
     <section className="flex w-full max-w-6xl flex-col pt-32">
       <div className="flex flex-col">
         {blogs.map((blog) => (
-          <>
+          <div key={blog.label}>
             <div className="flex h-20 w-full flex-row items-center justify-between pl-5 pr-10">
               <h1 className="text-4xl font-semibold text-[#414141] dark:text-white">
                 {convertName(blog.label)}
@@ -49,7 +49,7 @@ function BlogPage(): JSX.Element {
               </p>
             </div>
             <ul className="grid grid-cols-1 gap-8 md:grid-cols-auto">{renderPosts(blog.data)}</ul>
-          </>
+          </div>
         ))}
       </div>
     </section>
